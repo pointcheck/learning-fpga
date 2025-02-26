c01f0800e3d4b964474eef6e61145ce9a0d72ef3
Aquatic72 <akri2022v@gmail.com>
2025-02-26 20:26:34 +0500

    Renamed pwm_counter -> clock_counter, main_counter -> pwm_counter

--- MotorDRV/motor_drv.sv
+++ MotorDRV/motor_drv.sv
@@ -12,25 +12,25 @@ module motor_drv
 	output logic pwm_outB
 );
 
-	localparam integer CLK_DIV = clk_hz / pwm_hz;			// Calculating max number for clock divider (pwm_counter)
-	logic [$clog2(CLK_DIV)-1:0] pwm_counter = 0;			// Calculating bit width for clock divider (pwm_counter) to get 250 Hz frequency after dividing
-	logic [7:0] main_counter = 0;
+	localparam integer CLK_DIV = clk_hz / pwm_hz;			// Calculating max number for clock divider (clock_counter)
+	logic [$clog2(CLK_DIV)-1:0] clock_counter = 0;			// Calculating bit width for clock divider (clock_counter) to get 250 Hz frequency after dividing
+	logic [7:0] pwm_counter = 0;
 
 	always_ff @(posedge clk or posedge rst) begin
 		if (rst) begin
-			pwm_counter  <= 0;
-			main_counter <= 0;
+			clock_counter  <= 0;
+			pwm_counter <= 0;
 		end else if (enable) begin
-			pwm_counter <= pwm_counter + 1;
-			if (pwm_counter == CLK_DIV - 1) begin		// Resetting pwm_counter after it gets to the max number (CLK_DIV - 1)
-				pwm_counter <= 0;
-				main_counter <= main_counter + 1;	// Adding +1 to the main_counter to reach 250 Hz frequency for PWM signal
+			clock_counter <= clock_counter + 1;
+			if (clock_counter == CLK_DIV - 1) begin		// Resetting clock_counter after it gets to the max number (CLK_DIV - 1)
+				clock_counter <= 0;
+				pwm_counter <= pwm_counter + 1;		// Adding +1 to the pwm_counter to reach 250 Hz frequency for PWM signal
 			end
 		end
 	end
 
 	logic pwm;							
-	assign pwm = (main_counter < duty_cycle);			// Getting PWM signal
+	assign pwm = (pwm_counter < duty_cycle);			// Getting PWM signal
 	assign pwm_outA = direction ? pwm : 0;				// Setting the PWM signal to one of the outputs based on the direction variable
 	assign pwm_outB = direction ? 0 : pwm;
 
