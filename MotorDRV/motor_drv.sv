ef87e2898af86b373dc55463c06d8a6e808cdbb2
Aquatic72 <akri2022v@gmail.com>
2025-02-27 16:37:37 +0500

    Fixed wrong frequency for PWM signal

--- MotorDRV/motor_drv.sv
+++ MotorDRV/motor_drv.sv
@@ -12,8 +12,8 @@ module motor_drv
 	output logic pwm_outB
 );
 
-	localparam integer CLK_DIV = clk_hz / pwm_hz;			// Calculating max number for clock divider (clock_counter)
-	logic [$clog2(CLK_DIV)-1:0] clock_counter = 0;			// Calculating bit width for clock divider (clock_counter) to get 250 Hz frequency after dividing
+	localparam integer CLK_DIV = clk_hz / (256 *  pwm_hz);			// Calculating max number for clock divider (clock_counter)
+	logic [$clog2(CLK_DIV)-1:0] clock_counter = 0;			// Calculating bit width for clock divider (clock_counter) to get 64 kHz frequency after dividing
 	logic [7:0] pwm_counter = 0;
 
 	always_ff @(posedge clk or posedge rst) begin
@@ -24,13 +24,13 @@ module motor_drv
 			clock_counter <= clock_counter + 1;
 			if (clock_counter == CLK_DIV - 1) begin		// Resetting clock_counter after it gets to the max number (CLK_DIV - 1)
 				clock_counter <= 0;
-				pwm_counter <= pwm_counter + 1;		// Adding +1 to the pwm_counter to reach 250 Hz frequency for PWM signal
+				pwm_counter <= pwm_counter + 1;		// Adding +1 to the pwm_counter to reach 250 Hz frequency for PWM signal (64 kHz for pwm_counter)
 			end
 		end
 	end
 
 	logic pwm;							
-	assign pwm = (pwm_counter < duty_cycle);			// Getting PWM signal
+	assign pwm = (pwm_counter < duty_cycle);			// Getting PWM signal (250 Hz)
 	assign pwm_outA = direction ? pwm : 0;				// Setting the PWM signal to one of the outputs based on the direction variable
 	assign pwm_outB = direction ? 0 : pwm;
 
