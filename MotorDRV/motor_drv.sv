dd78e7b531ac18a458208a389a59f1a36c838cc3
Aquatic72 <akri2022v@gmail.com>
2025-03-07 20:05:57 +0500

    Fixed motor_drv.sv

--- MotorDRV/motor_drv.sv
+++ MotorDRV/motor_drv.sv
@@ -12,19 +12,23 @@ module motor_drv
 	output logic pwm_outB
 );
 
-	localparam integer CLK_DIV = clk_hz / (256 *  pwm_hz);			// Calculating max number for clock divider (clock_counter)
-	logic [$clog2(CLK_DIV)-1:0] clock_counter = 0;			// Calculating bit width for clock divider (clock_counter) to get 64 kHz frequency after dividing
-	logic [7:0] pwm_counter = 0;
+	localparam integer CLK_DIV = clk_hz / (256 *  pwm_hz);		// Calculating max number for clock divider (clock_counter)
+	logic [$clog2(CLK_DIV)-1:0] clock_counter;			// Calculating bit width for clock divider (clock_counter) to get 64 kHz frequency after dividing
+	logic [7:0] pwm_counter;
 
 	always_ff @(posedge clk or posedge rst) begin
 		if (rst) begin
 			clock_counter  <= 0;
-			pwm_counter <= 0;
+			pwm_counter <= 8'd0;
 		end else if (enable) begin
 			clock_counter <= clock_counter + 1;
 			if (clock_counter == CLK_DIV - 1) begin		// Resetting clock_counter after it gets to the max number (CLK_DIV - 1)
 				clock_counter <= 0;
+<<<<<<< HEAD
 				pwm_counter <= pwm_counter + 1;		// Adding +1 to the pwm_counter to reach 250 Hz frequency for PWM signal (64 kHz for pwm_counter)
+=======
+				pwm_counter <= pwm_counter + 8'd1;		// Adding +1 to the pwm_counter to reach 250 Hz frequency for PWM signal (64 kHz for pwm_counter)
+>>>>>>> cbf5d03 (Fixed motor_drv.sv)
 			end
 		end
 	end
