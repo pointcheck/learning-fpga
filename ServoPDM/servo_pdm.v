b750d9e6d127d34bffcb8cb0b4265a0bc36c951d
BuildTools <unconfigured@null.spigotmc.org>
2025-03-14 17:30:34 +0500

    Updated top.v, rewritten servo_pdm.v

--- ServoPDM/servo_pdm.v
+++ ServoPDM/servo_pdm.v
@@ -1,45 +1,41 @@
 module servo_pdm
 # (
-        parameter  clk_hz   = 25000000,						// Global clock
+        parameter  clk_hz   = 25000000,							// Global clock
         parameter  cyc_hz 	= 50,							// Frequency of produced PDM signal
 ) ( 
         input  wire       rst,								// Global RESET signal
         input  wire       clk,  							// Global Clock
         input  wire       en,   							// Enable signal
         input  reg [7:0]  duty, 							// input Duty cycle value
-        output wire        pdm								// produced PDM signal
+        output wire       pdm								// produced PDM signal
 );
 
-	localparam pdmw = (clk_hz*duty)/255;					// Calculating clocks number to count to needed PDM width
-	reg [$clog2(pdmw)-1:0] pdmw_counter;					// Calculating bit depth for PDM width counter
+	reg [15:0] pdm_width = duty*78 + 27500;						// Calculating clocks number to count to needed PDM width (+ 1,1ms at the start)
+	reg [15:0] pdmw_counter;							// PDM width counter
 
 	localparam divider = clk_hz/cyc_hz;						// Calculating clocks number to count to 20ms @ 25MHz
-	reg [$clog2(divider)-1:0] div_counter;					// Calculating bit depth for clocks number
+	reg [$clog2(divider)-1:0] div_counter;						// Calculating bit depth for clocks number
 	
 	wire pdm_done = 0;
 	
-	always @(posedge rst or posedge clk) begin				// Second counter makes 20ms pause after PDM signal
+	always @(posedge rst or posedge clk) begin					// Second counter makes 20ms pause after PDM signal
 		if (rst) begin
 			div_counter <= 0;
 			pdmw_counter <= 0;
 		end else if (en) begin
-			if (pdmw_counter <= pdmw and pdm_done == 0)		// Counter to make PDM signal (only when pdm_done = 0)
+			if ((pdm_width >= pdmw_counter) && (pdm_done == 0)) begin	// Counter to make PDM signal (only when pdm_done = 0)
 				pdmw_counter <= pdmw_counter + 1;
-			end else if (pdmw_counter > pdmw) begin			
-				pdm_done <= 1;								// Setting pdm_done to 1 when PDM signal is done
+			end else if (pdmw_counter > pdm_width) begin			
+				pdm_done <= 1;						// Setting pdm_done to 1 when PDM signal is done
 			end
 			if (pdm_done == 1) begin
 				div_counter <= div_counter + 1;				// Counting 20ms after PDM signal
 			end else if (div_counter == divider - 1)		
-				div_counter <= 0;							// Starting over when 20ms is over
+				div_counter <= 0;					// Starting over when 20ms is over
 				pdm_done <= 0;
 		end
 	end
 	
-	if (pdmw < pdmw_counter) begin							// Creating output signal
-		pdm <= 1;
-	end else begin
-		pdm <= 0;
-	end
+	wire pdm = (pdm_width > pdmw_counter) ? 0 : 1;
 	
 endmodule
--- ServoPDM/top.v
+++ ServoPDM/top.v
@@ -1,19 +1,18 @@
 module top (				// Declaring inputs and outputs according to the configuration file (karnix_cabga256.lpf)
 	input wire clk25,		// For testing, frequency of clk25 must be set lower in configuration file (25 KHz for example) to see different pulse width on LED
 	input wire [1:0] key,
-	output wire led
+	output wire [3:0] led 
 );
 	
 	servo_pdm # (			// Creating an instance of servo_pdm module (servo_pdm.sv)
 		.clk_hz(25000000),	 
 		.cyc_hz(50),
-		.pdm_hz(312500) 
-	) motor_inst (
+	) servo_inst (
 		.clk(clk25),
 		.en(key[0]),
 		.rst(key[1]),
-		.duty(8'd0),	// Duty cycle is manually set here for test purposes
-		.pdm(led)	// LED lights up showing pulse width
+		.duty(8'd0),		// Duty cycle is manually set here for test purposes
+		.pdm(led [0])		// LED lights up showing pulse width
 	);
 
 endmodule
