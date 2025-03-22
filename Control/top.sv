4316924620605cb1e1774b676411e8e6e3822dae
Aquatic72 <akri2022v@gmail.com>
2025-03-22 15:36:18 +0500

    Tried to make general top.sv file

--- Control/top.sv
+++ Control/top.sv
@@ -1,45 +1,74 @@
 module top (
-	input clk25,
-	inout logic [3:0] gpio,
-	input logic [3:0] led
+	input logic clk25,
+	input logic key[0],
+	inout logic [3:0] gpio
 );
 
-	// Объявим мотор
+	logic [7:0] motor_dc;
+	logic [7:0] servo_dc;
+	logic direction;
+
+        logic ir_ready;
+	logic ctl_valid;
+        logic [31:0] command;
+
+
+        control
+        # (
+                .clk_hz = 25000000,
+		.sclk_hz = 256
+        ) control_inst (
+		.clk(clk25),
+		.ir_ready(ir_ready),
+		.adc_ready(1'd0),
+		.command(command),
+		.can_move_fwd(1'd1),
+		.ctl_valid(ctl_valid),
+		.motor_dc(motor_dc),
+		.direction(direction),
+		.servo_dc(servo_dc)
+        );
+
+
+
 	motor_drv 
 	# (
-
+		.clk_hz = 25000000,
+		.pwm_hz = 250
 	) motor_inst (
-
+		.clk(clk25),
+		.enable(1'd1),
+		.rst(key[0]),
+		.direction(direction),
+		.duty_cycle(motor_dc),
+		.pwm_outA(gpio[1]),
+		.pwm_outB(gpio[2])
 	);
 
 
 
-	// Объявим серву
 	servo_pdm
 	# (
-
+		.clk_hz = 25000000,
+		.cyc_hz = 50
 	) servo_inst (
-
+		.rst(key[0]),
+		.clk(clk25),
+		.en(1'd1),
+		.duty(servo_dc),
+		.pdm(gpio[3])
 	);
 
+	
 
-
-	// Объявим декодер
-	ir_decoder
-	# (
-
-	) decoder_inst (
-
+	ir_decoder decoder_inst (
+		.clk(clk25),
+		.rst(key[0]),
+		.enable(ctl_valid),
+		.ir_input(gpio[0]),
+		.ready(ir_ready),
+		.command(command),
+		.test(1'd0)
 	);	
-
-
-
-	// Объявим контрол
-	control
-	# (
-		
-	) control_inst (
-
-	);
 
 endmodule
