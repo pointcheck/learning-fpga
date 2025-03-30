ce6d86e3a072b3211500d51f4b2faac5e6a8dca4
dok2003 <vankienc4gl@gmail.com>
2025-03-31 00:29:49 +0500

    ready decoder

--- IRDecoder/ir_decoder.v
+++ IRDecoder/ir_decoder.v
@@ -1,90 +1,97 @@
 module ir_decoder (
 	input wire clk,
-	input wire rst,
-	input wire enable,
-	input wire ir_input,
-	output wire ready,
-	output wire [31:0] command, 
-	output wire [3:0] test
+        input wire rst,
+	input wire ack,
+        input wire enable,
+        input wire ir_input,
+        output reg ready,
+        output wire [31:0] command,
 );
+	localparam T0_MIN = (28256-2816)/64;				//количество тактового сигнвла соотвествует сигнал 0 (время*чачтота = 1,13мс*25000000)
+	localparam T0_MAX = (28256+2816)/64;
+	localparam T1_MIN = (57008-5648)/64;				 //количество тактового сигнвла соотвествует сигнал 1(время*чачтота = 2.28мс*25000000)
+	localparam T1_MAX = (57008+5648)/64;
+	localparam START_MIN = (128176-6400)/64;			 //количество тактового сигнвла соотвествует сигнал старта
+	localparam START_MAX = (128176+6400)/64;
+	
+	reg [15:0] slow_clk_div;
+	wire slow_clk;
 
-	localparam T0_MIN = 12800 - 128;
-	localparam T0_MAX = 12800 + 128;
-	localparam T1_MIN = 40925 - 128;
-	localparam T1_MAX = 40925 + 128;
-	localparam START_MIN = 122500 - 128;
-	localparam START_MAX = 122500 + 128;
-	localparam T1_TOP = 1750000;
+        wire strobe_front;
 
-	reg ir_input_last;
-	wire strobe_front;
-	wire strobe_back;
-	reg [23:0] t1; 
-	reg [5:0] bit_count;
-	reg [31:0] cmd;
+	reg  ir_input_last;						//предыдущий бит 
+	reg [20:0] t1;							// счетчик для определения длинн бит-0 и бит-1
+	reg [7:0] bit_count;						//считать число быитов до 31
+	reg [31:0] cmd;							//32-битовый от пульта
 
-	reg test_start;
 
-	//assign test[3:0] = 4'b0;
-	assign ready = t1 == T1_TOP;
-	assign strobe_front = (ir_input_last != ir_input) & ir_input;
-	assign strobe_back = (ir_input_last != ir_input) & ~ir_input;
-
-	assign command[31:0] = cmd[31:0];
-
-	assign test[0] = strobe_front;
-	assign test[1] = strobe_back;
-	assign test[2] = test_start;
-	assign test[3] = 0;//1'b1;
-
-	always_ff @(posedge clk or posedge rst)
+	always @(posedge clk or posedge rst)
 	begin
 		if(rst) begin
-			ir_input_last <= '0;
-			t1 <= '0;
-			bit_count <= '0;
-
-			test_start <= '0;
-
+			slow_clk_div <= '0;
 		end else begin
-			if(enable)
-			begin
-				ir_input_last <= ir_input;
-
-				if(t1 != T1_TOP)
-					t1 <= t1 + '1;
-
-				if(strobe_front)
-					t1 <= '0;
-
-				if(strobe_back)
-				begin
-					if(t1 > START_MIN && t1 < START_MAX)
-					begin
-						// Start of packet
-						bit_count <= '0;
-						test_start <= '1;
-					end
-
-					if(t1 > T0_MIN && t1 < T0_MAX)
-					begin
-						// ZERO received
-						cmd[bit_count] <= '0;
-						bit_count <= bit_count + '1;
-					end
-
-					if(t1 > T1_MIN && t1 < T1_MAX)
-					begin
-						// ONE received
-						cmd[bit_count] <= '1;
-						bit_count <= bit_count + '1;
-					end
-
-				end
-
-
-			end
+			slow_clk_div <= slow_clk_div + 'd1;
 		end
 
 	end
+	
+	assign slow_clk = slow_clk_div[5];				//уменьшение частоты на 64 раз
+
+	assign command[31:0] = cmd[31:0];
+
+	assign strobe_front = (ir_input_last != ir_input) * ir_input; //условие для фронта тактового сигнала
+
+	always @(posedge slow_clk or posedge rst)
+        begin
+		if(rst) begin					//Сброс
+			ir_input_last <= 1'b1;
+		        t1 <= 'b0;
+			bit_count <= '0;
+			cmd<= '0;
+			ready <= 1'd0;
+
+                end else begin
+                        if(enable)
+                        begin	
+				
+				ir_input_last <= ir_input;
+				
+				if(strobe_front == 'b0)	 //проверить фронт тактового сингнал
+					t1<=t1+ 'b1;
+				else begin
+						
+					if(t1 == 21'h1fffff) begin //ограничения время
+						t1 <= '0;
+						bit_count <= '0;
+						cmd <= '0;
+					end
+
+				        if(t1>START_MIN  && t1<START_MAX) begin		// Стартовый сигнал
+						bit_count <= '0;
+						cmd <= '0;
+					end
+					
+					if(!ready) begin	//когда полная команда запишется то ready = 1
+					
+						if(t1<T0_MAX && t1>T0_MIN) begin	//Сигнал "0"
+							cmd = {1'b0, cmd[31:1]};
+							bit_count<=bit_count+'b1;	
+						end
+
+						if(t1>T1_MIN && t1<T1_MAX) begin	//Сигнал "1"
+							cmd = {1'b1, cmd[31:1]};
+							bit_count <= bit_count + 'b1;
+						end
+					end
+					t1<='b0;
+					if (bit_count == 31) begin
+						ready <= 1;
+					end
+				end
+				//if (ack == 1) ready <= 1'd0;	//Вспомогательный сигнал чтобы команнды не повториться
+
+                        end
+                end
+
+        end
 endmodule
