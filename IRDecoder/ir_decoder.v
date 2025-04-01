84a9364d410ddad4d365d6eb227910012d49ec4e
dok2003 <vankienc4gl@gmail.com>
2025-04-01 22:09:32 +0500

    123

--- IRDecoder/ir_decoder.v
+++ IRDecoder/ir_decoder.v
@@ -7,11 +7,11 @@ module ir_decoder (
         output reg ready,
         output wire [31:0] command,
 );
-	localparam T0_MIN = (28256-2816)/64;				//количество тактового сигнвла соотвествует сигнал 0 (время*чачтота = 1,13мс*25000000)
+	localparam T0_MIN = (28256-2816)/64;				//the amount of clock signal corresponds to signal 0 (time*frequency = 1.13 m s*25000000)
 	localparam T0_MAX = (28256+2816)/64;
-	localparam T1_MIN = (57008-5648)/64;				 //количество тактового сигнвла соотвествует сигнал 1(время*чачтота = 2.28мс*25000000)
+	localparam T1_MIN = (57008-5648)/64;				 //the number of clock signal corresponds to signal 1 (time * frequency = 2.28ms * 25000000)
 	localparam T1_MAX = (57008+5648)/64;
-	localparam START_MIN = (128176-6400)/64;			 //количество тактового сигнвла соотвествует сигнал старта
+	localparam START_MIN = (128176-6400)/64;			 //the amount of clock signal corresponds to the start signal
 	localparam START_MAX = (128176+6400)/64;
 	
 	reg [15:0] slow_clk_div;
@@ -19,10 +19,10 @@ module ir_decoder (
 
         wire strobe_front;
 
-	reg  ir_input_last;						//предыдущий бит 
-	reg [20:0] t1;							// счетчик для определения длинн бит-0 и бит-1
-	reg [7:0] bit_count;						//считать число быитов до 31
-	reg [31:0] cmd;							//32-битовый от пульта
+	reg  ir_input_last;						//last bit
+	reg [20:0] t1;							// counter for determining the length of bit-0 and bit-1
+	reg [7:0] bit_count;						//count the number of bits to 31
+	reg [31:0] cmd;							//32-bit from the remote
 
 
 	always @(posedge clk or posedge rst)
@@ -35,15 +35,15 @@ module ir_decoder (
 
 	end
 	
-	assign slow_clk = slow_clk_div[5];				//уменьшение частоты на 64 раз
+	assign slow_clk = slow_clk_div[5];				//frequency reduction by 64 times
 
 	assign command[31:0] = cmd[31:0];
 
-	assign strobe_front = (ir_input_last != ir_input) * ir_input; //условие для фронта тактового сигнала
+	assign strobe_front = (ir_input_last != ir_input) * ir_input; //clock front condition
 
 	always @(posedge slow_clk or posedge rst)
         begin
-		if(rst) begin					//Сброс
+		if(rst) begin					//reset
 			ir_input_last <= 1'b1;
 		        t1 <= 'b0;
 			bit_count <= '0;
@@ -56,29 +56,29 @@ module ir_decoder (
 				
 				ir_input_last <= ir_input;
 				
-				if(strobe_front == 'b0)	 //проверить фронт тактового сингнал
+				if(strobe_front == 'b0)	 //check clock front
 					t1<=t1+ 'b1;
 				else begin
 						
-					if(t1 == 21'h1fffff) begin //ограничения время
+					if(t1 == 21'h1fffff) begin //time restrictions
 						t1 <= '0;
 						bit_count <= '0;
 						cmd <= '0;
 					end
 
-				        if(t1>START_MIN  && t1<START_MAX) begin		// Стартовый сигнал
+				        if(t1>START_MIN  && t1<START_MAX) begin		// Start signal
 						bit_count <= '0;
 						cmd <= '0;
 					end
 					
-					if(!ready) begin	//когда полная команда запишется то ready = 1
+					if(!ready) begin	//when the full command is written then ready = 1
 					
-						if(t1<T0_MAX && t1>T0_MIN) begin	//Сигнал "0"
+						if(t1<T0_MAX && t1>T0_MIN) begin	//Signal "0"
 							cmd = {1'b0, cmd[31:1]};
 							bit_count<=bit_count+'b1;	
 						end
 
-						if(t1>T1_MIN && t1<T1_MAX) begin	//Сигнал "1"
+						if(t1>T1_MIN && t1<T1_MAX) begin	//Signal "1"
 							cmd = {1'b1, cmd[31:1]};
 							bit_count <= bit_count + 'b1;
 						end
@@ -88,7 +88,7 @@ module ir_decoder (
 						ready <= 1;
 					end
 				end
-				//if (ack == 1) ready <= 1'd0;	//Вспомогательный сигнал чтобы команнды не повториться
+				//if (ack == 1) ready <= 1'd0;	//An auxiliary signal to prevent commands from being repeated
 
                         end
                 end
