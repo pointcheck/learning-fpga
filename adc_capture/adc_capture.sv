c90877183f70d09b6c821193f3a29815b8c658ba
Aquatic72 <akri2022v@gmail.com>
2025-04-05 03:01:15 +0500

    Added adc_capture module with top.sv file and README.md, renamed ADCHysteresis to adc_hysteresis

---
+++ adc_capture/adc_capture.sv
@@ -0,0 +1,103 @@
+module adc_capture
+# (
+	parameter clk_hz = 25000000,						
+	parameter sclk_hz = 5000000,						// ADC128S052 polling frequency (3.2-8 MHz)
+	parameter cycle_pause = 10						// amount of slowed clocks (sclk) between ADC cycles  
+) (
+	input  logic clk,
+	input  logic rst,
+	input  logic ctl_valid,							// == 1 if control is ready to process d_signal
+	input  logic adc_ack,							// == 1 if control processed d_signal
+	input  logic [2:0] address,						// Address of ADC's analog input (IN0 - IN7)
+	input  logic dout_bit,							// DOUT port that sends converted digital signal
+	output logic sclk,							// Slowed clocks sended to ADC SCLK port
+	output logic cs,							// ADC's CS signal that begins the work cycle
+	output logic adc_ready,							// == 1 if ADC finished converting analog -> digital
+	output logic din_bit,							// DIN port to that adc_capture sends input address
+	output logic [11:0] d_signal						// Converted digital signal (12-bit)
+);
+
+	localparam MAX_DIV = clk_hz / (2 * sclk_hz) - 1;			// Dividing clk_hz to sclk_hz
+	logic [$clog2(MAX_DIV) - 1:0] frdiv;
+
+	always_ff @(posedge clk or posedge rst) begin
+
+		if (rst) begin
+			sclk <= 1'd0;
+			frdiv <= 'd0;
+		end else begin
+			if (frdiv >= MAX_DIV) begin
+				sclk <= ~sclk;
+			end else begin
+				frdiv <= frdiv + 'd1;
+			end
+		end
+
+	end
+
+	logic [7:0] din;						// Declaring what to send to the DIN port: 000 + address(3-bit) + 00
+	logic [15:0] dout;						// Declaring what to get from the DOUT port: 0000 + d_signal(12-bit)
+	assign d_signal = dout[11:0];
+
+	logic [3:0] cs_reg;						// cs_reg(4-bit) is used to count length of ADC's work cycle (16 slowed clocks sclk)
+	logic [$clog2(cycle_pause) - 1:0] cs_pause;			// cs_pause is used to count length of ADC's pause between work cycles (cycle_pause slowed clocks)
+
+	always_ff @(posedge sclk or posedge rst) begin
+
+		if (rst) begin						// Resetting all regs (not inputs except dout_bit)
+
+			adc_ready <= 1'd0;
+
+			cs_reg <= 4'd0;
+			cs_pause <= 'd0;
+			cs <= 1'd0;
+
+			din_bit <= 1'd0;
+			din <= 8'd0;
+
+			dout <= 16'd0;
+//			dout_bit <= 1'd0;
+//			d_signal <= 12'd0;
+
+		end else begin
+
+			if (cs) begin					// Performing single work cycle (cs = 1)
+
+				din <= {3'b000, address, 2'b00};
+
+				if (cs_reg < 8) begin
+					din_bit <= din[7 - cs_reg];	// Sending MSB to ADC DIN (000 + address + 00)
+				end
+
+				dout <= {dout[15:0], dout_bit};		// Recieving MSB from ADC DOUT and shifting it to the left (0000 + d_signal) 
+
+				if (&cs_reg) begin			// Clearing cs after 16 slowed clocks (end of work cycle)
+					cs <= 1'd0;
+					cs_reg <= 4'd0;
+					adc_ready <= 1'd1;		// Setting adc_ready after finishing work cycle
+
+				end else begin
+					cs_reg <= cs_reg + 1'd1;
+				end
+
+			end else begin					// Performing pause before another work cycle
+
+				if (cs_pause >= cycle_pause) begin	// Setting cs after cycle_pause slowed clocks
+					cs <= 1'd1;
+					cs_pause <= 'd0;
+
+				end else begin
+					cs_pause <= cs_pause + 1'd1;
+				end
+
+				if (adc_ack) begin
+					adc_ready <= 1'd0;		// Clearing adc_ready after control processed d_signal
+				end
+
+			end
+
+		end
+	
+	end
+
+endmodule
---
+++ adc_capture/top.sv
@@ -0,0 +1,98 @@
+module top
+# (
+	parameter sclk2_hz = 1
+) (
+	input  logic clk25,
+	input  logic [3:0] key,
+	input  logic adc_spi_miso,
+	output logic [3:0] led,
+	output logic adc_spi_mosi,
+	output logic adc_spi_sclk,
+	output logic adc_spi_csn
+);
+
+	logic rst;
+	logic ctl_valid;
+	logic adc_ack;
+	logic [2:0] address;
+	logic dout_bit;
+	logic sclk;
+	logic cs;
+	logic adc_ready;
+	logic din_bit;
+	logic [11:0] d_signal;
+
+	adc_capture # (
+		.clk_hz(25000000),
+		.sclk_hz(5000000),
+		.cycle_pause(10)
+	) adc_capture_inst (
+		.clk(clk25),
+		.rst(rst),
+		.ctl_valid(ctl_valid),
+		.adc_ack(adc_ack),
+		.address(address),
+		.dout_bit(dout_bit),
+		.sclk(sclk),
+		.cs(cs),
+		.adc_ready(adc_ready),
+		.din_bit(din_bit),
+		.d_signal(d_signal)
+	);
+
+	localparam CLOCK_DIV = 25000000 / (2 * sclk2_hz) - 1;
+	logic [$clog2(CLOCK_DIV) - 1:0] clkdiv;
+
+	logic sclk2;
+	always_ff @(posedge clk25 or posedge rst) begin
+
+		if (rst) begin
+			clkdiv <= 'd0;
+			sclk2 <= 1'd0;
+
+		end else if (clkdiv == CLOCK_DIV) begin
+			sclk2 <= ~sclk2;
+		end else begin
+			clkdiv <= clkdiv + 'd1;
+		end
+
+	end
+
+	always_ff @(posedge sclk2 or posedge rst) begin
+
+		if (rst) begin
+			adc_ack <= 1'd0;
+			ctl_valid <= 1'd1;
+			address <= 3'b000;			
+			
+		end else if (adc_ready) begin
+			adc_ack <= 1'd1;
+		end else begin
+			adc_ack <= 1'd0;
+		end
+
+	end
+
+	assign adc_spi_sclk = sclk;
+	assign adc_spi_mosi = din_bit;
+	assign adc_spi_csn = cs;
+	assign dout_bit = adc_spi_miso;
+
+	assign rst = key[3];
+
+	always_comb begin
+
+		case (key[2:0])
+
+			3'b001: led = d_signal[3:0];
+			3'b010: led = d_signal[7:4];
+			3'b011: led = d_signal[11:8];
+			3'b100: led = {1'b0, address};
+
+			default: led = {rst, cs, adc_ack, adc_ready};
+
+		endcase
+
+	end
+
+endmodule
